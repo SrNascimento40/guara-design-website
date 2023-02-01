9902d03f79e62381119a40dbddb2de5605388689
SrNascimento40 <wallacediogo10@hotmail.com>
2023-02-01 11:12:38 -0300

    feature: develop about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Container } from "./style";
+import { AboutText, Container, Title } from "./style";
 
 export default function About() {
   return (
     <Container>
-      <p>Abort!nh0</p>
+      <Title>Sobre nós</Title>
+      <AboutText>
+        A Guara Design é uma empresa inovadora que surgiu em 2023 com a missão
+        de levar as pequenas empresas para o mundo digital.
+        <br />
+        Com uma equipe especializada em desenvolvimento de sites, design e
+        marketing digital, a Guara oferece soluções completas para garantir o
+        crescimento e sucesso dos seus clientes.
+      </AboutText>
     </Container>
   );
 }
--- src/pages/About/style.ts
+++ src/pages/About/style.ts
@@ -1,8 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100%;
+  height: 100vh;
   width: 100%;
   background-color: #262626;
-  color: #d9ad77;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 0 30%;
+`;
+
+export const Title = styled.h2`
+  color: #f2f2f2;
+  font-size: 4rem;
+  font-weight: 800;
+
+  margin-bottom: 1.5rem;
+`;
+
+export const AboutText = styled.p`
+  color: #e9e9e9;
+  font-size: 1.125rem;
+  font-weight: 300;
+  line-height: 1.75rem;
 `;
