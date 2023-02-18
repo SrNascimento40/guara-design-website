c574ec9b5e8099bdd539796c7fa1bec67942220d
SrNascimento40 <wallacediogo10@hotmail.com>
2023-02-17 22:14:03 -0300

    feature: create admin pages

---
+++ src/pages/post/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+export default function Post() {
+  return (
+    <Container>
+      <a href="#">file</a>
+      <a href="#">frango</a>
+      <a href="#">tit</a>
+    </Container>
+  );
+}
